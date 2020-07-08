a137c3f4d2ee0edd451772005a034d1a3fc3e20f
Yasunari t <u7df25c588m737@gmail.com>
2020-07-08 17:27:26 +0900

    🔥feat: created message function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core"
+import firebase from "firebase"
+import FlipMove from "react-flip-move"
+// import firebase from "firebase"
+// CSS
+import "./App.css"
+// Components
+import Message from "./components/messenger/Message"
+// Plugins
+import { db } from "./plugins/firebase"
 
 function App() {
+  const [input, setInput] = useState("")
+  const [messages, setMessages] = useState([
+    { userName: "Test Man", text: "hello" },
+  ])
+  const [userName, setUserName] = useState("")
+
+  // useEffect = run code on a condition in REACT
+  useEffect(() => {
+    setUserName(prompt("Please enter your name"))
+    db.collection("messages")
+      .orderBy("timeStamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        )
+      })
+  }, [])
+
+  const sendMessege = (event) => {
+    event.preventDefault()
+    // firestore
+    db.collection("messages").add({
+      text: input,
+      userName: userName,
+      timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    setInput("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testify🔥: Hello {userName}</h1>
+
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            type="submit"
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            onClick={sendMessege}
+          >
+            Send Messege
+          </Button>
+        </FormControl>
+      </form>
+
+      <FlipMove>
+        {messages.map((message) => (
+          <Message key={message.id} message={message} userName={userName} />
+        ))}
+      </FlipMove>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/messenger/Message.jsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from "react"
+import { Card, CardContent, Typography } from "@material-ui/core"
+// css
+import "./Message.css"
+const Message = forwardRef(({ message, userName }, ref) => {
+  const isUser = userName === message.userName
+
+  return (
+    <div ref={ref} className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent>
+          <Typography variant="h5" color="white" component="h2">
+            {message.userName}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
+})
+export default Message
